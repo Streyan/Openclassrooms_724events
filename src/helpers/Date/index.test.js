0fcdd75ff3f6918c85be221a76b7c1d18bd01790
Streyan <leplongeon.celine@gmail.com>
2025-02-06 17:14:24 +0100

    fix slider part 1

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,21 @@
-/**
- * 
- */
+import { getMonth } from ".";
 
 describe("Date helper", () => {
-    describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
-        });
+  describe("When getMonth is called", () => {
+    it("the function return janvier for 2022-01-01 as date", () => {
+      expect(getMonth(new Date("2022-01-01"))).toBe("janvier");
     });
-})
-
+    it("the function return juillet for 2022-07-08 as date", () => {
+      expect(getMonth(new Date("2022-07-08"))).toBe("juillet");
+    });
+    it("the function return janvier for 2022-01-29T20:28:45.744Z as date", () => {
+      expect(getMonth(new Date("2022-01-29T20:28:45.744Z"))).toBe("janvier");
+    });
+    it("the function return mars for 2022-03-29T20:28:45.744Z as date", () => {
+      expect(getMonth(new Date("2022-03-29T20:28:45.744Z"))).toBe("mars");
+    });
+    it("the function return mai for 2022-05-29T20:28:45.744Z as date", () => {
+      expect(getMonth(new Date("2022-05-29T20:28:45.744Z"))).toBe("mai");
+    });
+  });
+});
